994573320638a91431f4e3af38ee79e26968e633
nico.botha <botha.nicow@gmail.com>
2018-07-08 23:38:34 +0200

    Clean up of demo app

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
-// import ReactMobileLightbox from '../../';
-import ReactMobileLightbox from './lightbox/index';
+import './App.css';
+import ReactImageVideoLightbox from '../../';
 
 class App extends Component {
+
+  constructor() {
+    super(...arguments);
+
+    this.state = {
+      lightboxOpen: false
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <ReactMobileLightbox
-          width={window.innerWidth}
-          height={window.innerHeight}
-          imageUrls={['https://placekitten.com/450/300', 'https://placekitten.com/420/350', 'https://placekitten.com/550/500']}
-          startIndex={0} />
+      <div className="container">
+        <button onClick={() => { this.setState({ lightboxOpen: true }); }}>Open Lightbox</button>
+        {
+          this.state.lightboxOpen &&
+          <ReactImageVideoLightbox
+            data={[
+              { url: 'https://placekitten.com/450/300', type: 'photo', altTag: 'placeholder image' },
+              { url: 'https://www.youtube.com/embed/ScMzIvxBSi4?rel=0', type: 'video', altTag: 'placeholder video' },
+              { url: 'https://placekitten.com/420/350', type: 'photo', altTag: 'placeholder image' },
+              { url: 'https://placekitten.com/550/500', type: 'photo', altTag: 'placeholder image' },
+              { url: 'https://www.youtube.com/embed/ScMzIvxBSi4?rel=0', type: 'video', altTag: 'placeholder video' }]}
+            startIndex={0}
+            onCloseCallback={() => { this.setState({ lightboxOpen: false }); }} />
+        }
       </div>
     );
   }
