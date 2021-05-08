737f1e3fe574efdf600f6b99de7cc2e4cf2fc447
Ngineer101 <nicolaasb@pm.me>
2021-05-08 14:43:23 +0200

    Removed material UI and added native svgs

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,5 @@
 import * as React from 'react';
 import * as utils from './utils';
-import NavigateBefore from '@material-ui/icons/NavigateBefore';
-import NavigateNext from '@material-ui/icons/NavigateNext'
-import Close from '@material-ui/icons/Close';
-import { CircularProgress } from '@material-ui/core';
 
 const MIN_SCALE = 1;
 const MAX_SCALE = 4;
@@ -336,7 +332,7 @@ class ReactImageVideoLightbox extends React.Component<IProps, IState> {
           </div>
         }
 
-        <Close
+        <div
           style={{
             position: 'absolute',
             top: '0px',
@@ -346,10 +342,15 @@ class ReactImageVideoLightbox extends React.Component<IProps, IState> {
             cursor: 'pointer',
             fontSize: `${this.state.iconSize * 0.8}px`
           }}
-          onClick={this.props.onCloseCallback} />
+          onClick={this.props.onCloseCallback}>
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF">
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+          </svg>
+        </div>
         {
           (this.state.index + 1 != 1) ?
-            <NavigateBefore
+            <div
               style={{
                 position: 'absolute',
                 left: '0px',
@@ -358,13 +359,18 @@ class ReactImageVideoLightbox extends React.Component<IProps, IState> {
                 cursor: 'pointer',
                 fontSize: `${this.state.iconSize}px`
               }}
-              onClick={() => { this.swipeLeft(); }} />
+              onClick={() => { this.swipeLeft(); }}>
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF">
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
+              </svg>
+            </div>
             :
             <></>
         }
         {
           (this.state.index + 1 != this.props.data.length) ?
-            <NavigateNext
+            <div
               style={{
                 position: 'absolute',
                 right: '0px',
@@ -373,17 +379,20 @@ class ReactImageVideoLightbox extends React.Component<IProps, IState> {
                 cursor: 'pointer',
                 fontSize: `${this.state.iconSize}px`
               }}
-              onClick={() => { this.swipeRight(); }} />
+              onClick={() => { this.swipeRight(); }}>
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF">
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" />
+              </svg>
+            </div>
             :
             <></>
         }
         {
           this.state.loading &&
-          <CircularProgress
-            style={{
-              position: 'absolute',
-              color: '#FFFFFF'
-            }} />
+          <>
+          {/* TODO: Add spinner */}
+          </>
         }
 
         {
